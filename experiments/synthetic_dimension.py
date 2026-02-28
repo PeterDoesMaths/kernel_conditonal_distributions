f60ce3086ec685770f3cb28c79531c2ca4b09531
Peter Moskvichev <peter.mosk123@gmail.com>
2026-02-28 16:42:01 +1030

    update dynthetic dimension plot

--- experiments/synthetic_dimension.py
+++ experiments/synthetic_dimension.py
@@ -39,7 +39,7 @@ def sample_joint_theta_multidim(
 			n,
 			theta,
 			conditional_fn,
-			noise_std=0.1*(d+1)*noise_std,
+			noise_std=(0.1*d+1)*noise_std,
 			seed=seed_d,
 		)
 		X_cols.append(X_d)
